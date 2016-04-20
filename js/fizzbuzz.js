1d0a09793763c73144c13d4a751ea8b210532f2b
Yonathan Ayenew <yonayen@gmail.com>
2016-04-20 08:28:37 -0700

    before getting it to work

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,29 +1,39 @@
-var userNumber = prompt("Hi there!  Pick a Number");
-var notString;
+$(document).load(function (){
 
-	if (userNumber != null) {
-		var notString = parseInt(userNumber)
+
+	var userSuggested = prompt("Hi there!  Pick a Number");
+
+	// convert userSuggested into  integer from string (+22)
+	// function takes integer and returns fizzbuzz until that value
+
+	function stringToNumber () {
+		var finalNumber = parseInt(userSuggested);
 	};
 
-	
+
+	// var notString;
+
+	// 	if (userNumber != null) {
+	// 		var notString = parseInt(userNumber)
+	// 	};
 
 
-
-$(document).ready(function() {
-	for (var number = 1; number < 100; number++) {
-		if (number%5 == 0 && number%3 == 0) {
-			document.write('fizz buzz' + '<br>');
+	function fizzbuzz (finalNumber) {
+		for (var number = 1; number < 100; number++) {
+			if (number%5 == 0 && number%3 == 0) {
+				document.write('fizz buzz' + '<br>');
+			}
+			else if (number%3 == 0) {
+				document.write('fizz' + '<br>');
+			}
+			else if (number%5 == 0) {
+				document.write('buzz' + '<br>');
+			}
+			else {
+			document.write(number + '<br>');
+			}
 		}
-		else if (number%3 == 0) {
-			document.write('fizz' + '<br>');
-		}
-		else if (number%5 == 0) {
-			document.write('buzz' + '<br>');
-		}
-		else {
-		document.write(number + '<br>');
-		}
-	}
+};
 });
 
 
@@ -46,18 +56,4 @@ $(document).ready(function() {
  //  6- Don't design app.
 
 
-
-
-
-
-
- // -- Old Assignment --//
-
-// Count from 1-100; just say number if it's not divisible by 3 or 5.  
-// If divisible by 3 --> 'fizz'
-// If divisible by 5 --> 'buzz'
-// If dibisible by both 3 & 5 --> 'fizzbuzz'
-// 1, 2, fizz, 4, buzz, fizz, 7,8,fizz, buzz, 11, fizz, 13, 14, fizz buzz
-
- // ------- //
 
